953d0c7d76f23c58404e8a80502a143928354004
wangyang <60211781@qq.com>
2019-06-23 17:33:10 +0800

    s

--- PhotoCompose.Web/Controllers/HomeController.cs
+++ PhotoCompose.Web/Controllers/HomeController.cs
@@ -25,15 +25,15 @@ namespace PhotoCompose.Web.Controllers
             {
                 {"type", "foreground"}
             };
-            var result = client.BodySeg(file.InputStream.ToBytes(),options);
+            var result = client.BodySeg(file.InputStream.ToBytes(), options);
             var imageBase64 = result["foreground"].ToString();
             byte[] imageBytes = imageBase64.Base64StrToBuffer();
             MemoryStream imageBkStream = new MemoryStream();
             imageBkStream.Write(imageBytes, 0, imageBytes.Length);
-            Image imageBack = Image.FromStream(imageBkStream);
+            Bitmap img = new Bitmap(imageBkStream);
             //背景图
-            Image img = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + template);
-            var newImage = CombinImage(img, imageBack);
+            Image imageBack = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + template);
+            var newImage = CombinImage(imageBack, img);
             MemoryStream memoryStream = new MemoryStream();
             newImage.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
             memoryStream.Position = 0;
@@ -47,21 +47,50 @@ namespace PhotoCompose.Web.Controllers
         /// <param name="xDeviation"></param>
         /// <param name="yDeviation"></param>
         /// <returns></returns>
-        public static Bitmap CombinImage(Image imgBack, Image img, int xDeviation = 0, int yDeviation = 0)
+        public static Bitmap CombinImage(Image imageBack, Bitmap img, int xDeviation = 0, int yDeviation = 0)
         {
-            Bitmap bmp = new Bitmap(imgBack.Width, imgBack.Height);
+            Bitmap bmp = new Bitmap(imageBack.Width, imageBack.Height);
 
             Graphics g = Graphics.FromImage(bmp);
             g.Clear(Color.White);
-            g.DrawImage(imgBack, 0, 0, imgBack.Width, imgBack.Height);
-
-            g.DrawImage(img, imgBack.Width / 2 - img.Width / 2 + xDeviation, imgBack.Height / 2 - img.Height / 2 + yDeviation, img.Width, img.Height);
+            g.DrawImage(imageBack, 0, 0, imageBack.Width, imageBack.Height);
+            Dictionary<int, List<int>> imgs = new Dictionary<int, List<int>>();
+            for (var x = 0; x < img.Width; x++)
+            {
+                for (var y = 0; y < img.Height; y++)
+                {
+                    var p = img.GetPixel(x, y);
+                    if (p.A > 0)
+                    {
+                        if (imgs.ContainsKey(x)) {
+                            imgs[x].Add(y);
+                        }
+                        else
+                        {
+                            imgs.Add(x, new List<int>());
+                        }
+                    }
+                }
+            }
+            int minX = img.Width, minY = img.Height;
+            int maxX = 0, maxY = 0;
+            int realWidth = maxX - minX;
+            int realHeight = maxY - minY;
+            foreach(var kv in imgs)
+            {
+                if (kv.Key < minX) minX = kv.Key;
+                if (kv.Key > maxX) maxX = kv.Key;
+                if (kv.Value.Min() < minY) minY = kv.Value.Min();
+                if (kv.Value.Max() > maxY) maxY = kv.Value.Max();
+            }
+            int x = imageBack.Width< realWidth?0:
+            g.DrawImage(img, 0, 0, img.Width / 2, img.Height / 2);
             GC.Collect();
             return bmp;
         }
         public ActionResult Get()
         {
-            FileStream fileStream = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "images\\background\\classical\\01.jpg",FileMode.Open,FileAccess.Read);
+            FileStream fileStream = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "images\\background\\classical\\01.jpg", FileMode.Open, FileAccess.Read);
             return File(fileStream, "image/png");
         }
 
